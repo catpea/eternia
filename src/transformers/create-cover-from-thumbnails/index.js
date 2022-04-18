888f5365603a263ae52ceaa05e4253609c90246c
catpea <corporate.miou@gmail.com>
2022-04-17 20:31:27 -0400

    New Release

--- src/transformers/create-cover-from-thumbnails/index.js
+++ src/transformers/create-cover-from-thumbnails/index.js
@@ -13,6 +13,10 @@ import yaml from "js-yaml";
 import { writeFile, readFile } from "fs/promises";
 import { exists, expired, ydbImages } from "../helpers.js";
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 export default main;
 
 // to be ran after changes to record object are made
@@ -30,25 +34,30 @@ async function main({record, project, home}){
   const database = yaml.load(await readFile(yamlContentFile));
 
   const destinationFile = path.join(filesDirectory, record.image);
-  const sourceFiles = (await ydbImages(database)).map(i=>path.join(filesDirectory,i));
+  const sourceFiles = (await ydbImages(database)).filter(i=>i.endsWith('.jpg')).map(i=>path.join(filesDirectory,i));
+  // const sourceFiles = (await ydbImages(database)).map(i=>path.join(filesDirectory,i));
 
   if(sourceFiles.length === 0) return;
 
   const latestFile = sourceFiles.map(file=>({file, date: new Date(statSync(file).mtime)})).sort((a, b) => b.date - a.date).shift().file;
 
-    if(await expired(destinationFile, [latestFile,yamlContentFile])){
+    if(await expired(destinationFile, [latestFile, yamlContentFile])){
 
     debug(`rebuilding cover image for: ${record.id}`);
-
+    //console.log(sourceFiles);
+    //await sleep(50000)
     let tile = 3;
+
+    //BUG: sourceFiles should be sourceFiles.length # so the default is always 3
+    // ## this will force a large rebuild and cause websites to fail, I am leving the bug in here for now.
     if(sourceFiles > 17) tile = 3;
     if(sourceFiles > 25) tile = 4;
     if(sourceFiles > 35) tile = 5;
     if(sourceFiles > 45) tile = 7;
-    if(sourceFiles < 9) tile = 2;
+    if(sourceFiles < 12) tile = 2;
     if(sourceFiles < 5) tile = 2;
     if(sourceFiles < 3) tile = 2;
-    
+
 
     const command = 'montage';
     const commandArguments = [
